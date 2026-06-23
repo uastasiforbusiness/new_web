37f2d23b28edc05e1965f3bef9445b147834de87
uastasiforbusiness <uastasiforbusiness@gmail.com>
2026-06-23 14:14:43 -0600

    fix: conditional render hero content, no preload flash

--- src/app/_components/home-client.tsx
+++ src/app/_components/home-client.tsx
@@ -64,7 +64,7 @@ export function HomeClient() {
         {!loaded && <LoadingScreen onComplete={handleLoadComplete} />}
       </AnimatePresence>
 
-      <div style={{ visibility: loaded ? 'visible' : 'hidden' }}>
+      {loaded && <div>
         <Navigation />
         <HeroScaleDown />
         <MarqueeText />
