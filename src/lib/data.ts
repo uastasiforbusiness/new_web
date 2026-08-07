cd75e03195607a5fb6334a2d5e7ed1b6dedd8c4f
uastasiforbusiness <2.49519752e+08+uastasiforbusiness@users.noreply.github.com>
2026-08-07 07:15:00 +0000

    Use a white Ferrari California image

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -46,7 +46,7 @@ export const CARS: Car[] = [
     name: "Ferrari California",
     kind: "Convertible GT",
     tagline: "The naturally-aspirated icon that started it all.",
-    image: "/images/ferrari_blanca_card.webp",
+    image: "/images/ferrari_california_white.jpg",
     price: "€1,950",
     priceNote: "per day",
     seats: "2",
